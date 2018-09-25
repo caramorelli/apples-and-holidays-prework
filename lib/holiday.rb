44f4d8dadb7793383792f37cf5d2cca47abed9bb
Cara Morelli <caramorelli7@gmail.com>
2018-09-25 02:48:12 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,23 +69,6 @@ def all_supplies_in_holidays(holiday_hash)
   end 
       
 end
-holiday_hash = {
-    :winter => {
-      :christmas => ["Lights", "Wreath"],
-      :new_years => ["Party Hats"]
-    },
-    :summer => {
-      :fourth_of_july => ["Fireworks", "BBQ"]
-    },
-    :fall => {
-      :thanksgiving => ["Turkey"]
-    },
-    :spring => {
-      :memorial_day => ["BBQ"]
-    }
-  }
-  
- all_supplies_in_holidays(holiday_hash) 
 
 
 
