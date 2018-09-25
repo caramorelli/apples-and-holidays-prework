925e2ab7cbbc3b4e627e37d52a8c14d32ef630e0
Cara Morelli <caramorelli7@gmail.com>
2018-09-25 03:09:33 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,9 +68,9 @@ def all_supplies_in_holidays(holiday_hash)
       new_k = k.to_s
       if new_k.chars.include?('_')
         new_k = new_k.chars.collect! { |char| char.replace(' ') if char == '_'  }
-      else 
-        new_k = new_k.capitalize! + ': ' + v.join(', ')
       end 
+      puts '  ' + new_k.capitalize! + ': ' + v.join(', ')
+      
     end 
   end 
       
